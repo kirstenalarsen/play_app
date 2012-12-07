be415e57337174bce0a4a794c3330f1d84feba17
kirstenalarsen <kirsten@eaterprises.com.au>
2012-12-08 08:44:00 +1100

    Add a StaticPages Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 PlayApp::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
+  get "static_pages/map"
+
+  get "static_pages/graph"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

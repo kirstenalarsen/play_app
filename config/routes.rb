11c32c2b06bccb70b4ccb940a8ebd0d3beef4f98
kirstenalarsen <kirsten@eaterprises.com.au>
2012-12-15 19:32:21 +1100

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 PlayApp::Application.routes.draw do
+  get "users/new"
+
+  root to: 'static_pages#home'
   
-  get "static_pages/home"
-
-  get "static_pages/help"
-
-  get "static_pages/map"
-
-  get "static_pages/graph"
+  match "/signup", to: 'users#new'
   
-  get "static_pages/about"
+  match "/help", to: 'static_pages#help' 
+  match "/map", to: 'static_pages#map' 
+  match "/graph", to: 'static_pages#graph' 
+  match "/about", to: 'static_pages#about' 
+  match "/contact", to: 'static_pages#contact' 
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,67 +3,40 @@ require 'spec_helper'
 describe "StaticPages" do
   
   describe "Home page" do
-    it "should have the right title" do
-      visit '/static_pages/home'
-      page.should have_selector('title',
-                        :text => "Play App | Home")
+    before { visit root_path } 
+    subject { page }
+    it { should have_selector('h1', text: 'Ripe') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector('title', text: '|Home' ) }
     end
-    it "should have the content 'Play App'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-      page.should have_selector('h2', :text => 'Play App')
-    end
-  end
   
   describe "Help page" do
-     it "should have the right title" do
-       visit '/static_pages/help'
-       page.should have_selector('title',
-                         :text => "Play App | Help")
-     end
-     it "should have the content 'Help Me'" do
-       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-       visit '/static_pages/help'
-       page.should have_content('Help Me')
-     end
-   end
+    before { visit help_path }
+    subject { page }
+    it { should have_selector('title', text: full_title('Help')) }
+    it { should have_content ('Help Me') }
+    end
+
    
    describe "Maps page" do
-      it "should have the right title" do
-         visit '/static_pages/map'
-         page.should have_selector('title',
-                           :text => "Play App | Maps")
-       end
-       it "should have the content 'Maps'" do
-        # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-        visit '/static_pages/map'
-        page.should have_content('Map')
-      end
+    before { visit map_path } 
+    subject { page }
+    it { should have_selector('title', text: full_title('Maps')) }
+    it { should have_content ('Map') }
     end
-    
-    describe "Graphs page" do
-       it "should have the right title" do
-          visit '/static_pages/graph'
-          page.should have_selector('title',
-                            :text => "Play App | Graph")
-        end
-        it "should have the content 'Graph'" do
-         # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-         visit '/static_pages/graph'
-         page.should have_content('Graph')
-       end
-     end
      
-       describe "About page" do
-            it "should have the right title" do
-                visit '/static_pages/about'
-                page.should have_selector('title',
-                                  :text => "Play App | About")
-              end
-              it "should have the content 'About Us'" do
-            # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-            visit '/static_pages/about'
-            page.should have_content('About Us')
-          end
-        end
+  describe "About page" do
+    before { visit about_path } 
+    subject { page }
+    it { should have_selector('title', text: full_title('About')) }
+    it { should have_content ('About') }
+    end
+     
+  describe "Contact page" do
+    before { visit contact_path } 
+    subject { page }
+    it { should have_selector('title', text: full_title('Contact')) }
+    it { should have_content ('Contact') }
+    end
+         
 end
